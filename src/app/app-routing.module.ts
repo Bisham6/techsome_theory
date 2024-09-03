02efe2c53ef38ce3b510beadea6ea577aa4fa9a3
pandeysury <pandeysury1998@gmail.com>
2024-09-03 22:36:22 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './Shared Component/header/header.component';
+import { FooterComponent } from './Shared Component/footer/footer.component';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations:[]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { SolutionsComponent } from './pages/solutions/solutions.component';
+import { ContectComponent } from './pages/contect/contect.component';
+import { SharedComponentModuleModule } from './Shared Component/shared-component-module/shared-component-module.module';
+import { HeaderComponent } from './Shared Component/header/header.component';
+import { FooterComponent } from './Shared Component/footer/footer.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ServicesComponent,
+    SolutionsComponent,
+    ContectComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedComponentModuleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
